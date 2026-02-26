08af1ddf86b5afef342dbf79e72645abc5c109d9
zidane <zidanegm@live.com>
2026-02-26 13:33:06 -0300

    plantar agora ve se tem semente e tira se plantar

--- Assets/Scripts/plant/confirmSeed.cs
+++ Assets/Scripts/plant/confirmSeed.cs
@@ -13,11 +13,18 @@ public class confirmSeed : MonoBehaviour
 
     public void sendSeed() //função chamada quando clica na semente
     {
-        
-        selectSlot.GetComponent<plant>().seedPlant = thisSeed; // passa o valor int da semente selecionada
-        selectSlot.GetComponent<plant>().plantDay = GameObject.FindWithTag("GameController").GetComponent<playerController>().dayCount; // joga o dia que a semente foi plantada para o script da planta, para depois comparar com o dia atual do jogo e fazer a planta crescer
-        selectSlot.GetComponent<plant>().nowDay = GameObject.FindWithTag("GameController").GetComponent<playerController>().dayCount; // joga o dia atual do jogo para o script da planta, para comparar com o dia que a planta foi plantada
-        selectSlot.GetComponent<plant>().glowDay = 1; //define o dia de crescimento da planta como 1, para que ela apareça no sprite correto
-        transform.parent.gameObject.SetActive(false); //desativa a tela de seleção de semente, o objeto pai a esse
+        if(GameObject.FindWithTag("GameController").GetComponent<playerController>().seedCount[thisSeed-1] >= 1) //verifica se o jogador tem a semente selecionada, se não tiver, a função é cancelada
+        {   
+            selectSlot.GetComponent<plant>().seedPlant = thisSeed; // passa o valor int da semente selecionada
+            selectSlot.GetComponent<plant>().plantDay = GameObject.FindWithTag("GameController").GetComponent<playerController>().dayCount; // joga o dia que a semente foi plantada para o script da planta, para depois comparar com o dia atual do jogo e fazer a planta crescer
+            selectSlot.GetComponent<plant>().nowDay = GameObject.FindWithTag("GameController").GetComponent<playerController>().dayCount; // joga o dia atual do jogo para o script da planta, para comparar com o dia que a planta foi plantada
+            selectSlot.GetComponent<plant>().glowDay = 1; //define o dia de crescimento da planta como 1, para que ela apareça no sprite correto
+            transform.parent.gameObject.SetActive(false); //desativa a tela de seleção de semente, o objeto pai a esse
+            GameObject.FindWithTag("GameController").GetComponent<playerController>().seedCount[thisSeed-1]--; //remove uma semente do jogador
+        }
+        else
+        {
+            // se n tiver semente
+        }
     }
 }
--- Assets/Scripts/player/playerController.cs
+++ Assets/Scripts/player/playerController.cs
@@ -10,11 +10,25 @@ public class playerController : MonoBehaviour
 
     //quantidade de sementes e plantas que o jogador tem, pode ser acessada por outros scripts para verificar se o jogador tem a quantidade necessária para plantar ou vender
     [SerializeField] public TextMeshProUGUI []seeds; //array com o texto de cada semente para atualizar a quantidade na UI
+    
+    
     [SerializeField] public int carrotSeed;
     [SerializeField] public int cornSeed;
     [SerializeField] public int eggplantSeed;
     [SerializeField] public int wheatSeed;
     [SerializeField] public int pumpkinSeed;
+
+    [SerializeField] public int []seedCount; //array com a quantidade de cada semente que o jogador tem
+    int seedCountLength = 5; //quantidade de tipos de sementes, para definir o tamanho do array
+    void Start()
+    {
+        seedCount = new int[seedCountLength];
+        seedCount[0] = carrotSeed;
+        seedCount[1] = cornSeed;  
+        seedCount[2] = eggplantSeed;
+        seedCount[3] = wheatSeed;
+        seedCount[4] = pumpkinSeed;
+    }
 
     //quantidade de plantas que o jogador tem, pode ser acessada por outros scripts para verificar se o jogador tem a quantidade necessária para vender
     [SerializeField] public TextMeshProUGUI []plants; //array com o texto de cada planta para atualizar a quantidade na UI
@@ -28,12 +42,19 @@ public class playerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        //texto na ui da mochila
         seeds[0].text = carrotSeed.ToString();
         seeds[1].text = cornSeed.ToString();
         seeds[2].text = eggplantSeed.ToString();
         seeds[3].text = wheatSeed.ToString();
         seeds[4].text = pumpkinSeed.ToString();
+        //texto na ui da plantação
+        seeds[5].text = carrotSeed.ToString();
+        seeds[6].text = cornSeed.ToString();
+        seeds[7].text = eggplantSeed.ToString();
+        seeds[8].text = wheatSeed.ToString();
+        seeds[9].text = pumpkinSeed.ToString();
 
         plants[0].text = carrot.ToString();
         plants[1].text = corn.ToString();
@@ -42,7 +63,13 @@ public class playerController : MonoBehaviour
         plants[4].text = pumpkin.ToString();
 
         money.text = totalMoney.ToString();
-        
+
+        //variavel com valor de semente recebe o valor atualizado do array
+        carrotSeed = seedCount[0];
+        cornSeed = seedCount[1];            
+        eggplantSeed = seedCount[2];
+        wheatSeed = seedCount[3];
+        pumpkinSeed = seedCount[4];
     
     }
 
